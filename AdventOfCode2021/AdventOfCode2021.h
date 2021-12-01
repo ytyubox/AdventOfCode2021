c78673ad6e9d66c92b6419a8c8a33d9c6619ffb2
Tsungyu Yu <ytyubox@gmail.com>
2021-12-01 20:22:18 +0800

    Initial Commit

---
+++ AdventOfCode2021/AdventOfCode2021.h
@@ -0,0 +1,21 @@
+//
+/* 
+ *		Created by 游宗諭 in 2021/12/1
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 12.0
+ */
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdventOfCode2021.
+FOUNDATION_EXPORT double AdventOfCode2021VersionNumber;
+
+//! Project version string for AdventOfCode2021.
+FOUNDATION_EXPORT const unsigned char AdventOfCode2021VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdventOfCode2021/PublicHeader.h>
+
+
