a6048c51e29ba1084af4077347093e6c9a67a6ac
Tsungyu Yu <ytyubox@gmail.com>
2021-12-12 17:42:58 +0800

    day 12 part 2

--- AdventOfCode2021Tests/Day12.swift
+++ AdventOfCode2021Tests/Day12.swift
@@ -10,10 +10,10 @@ final class Day12Tests: XCTestCase {
         page1(input: slightlyLargerInput, shouldBe: 19)
         page1(input: evenLargerExample, shouldBe: 226)
         page1(input: input, shouldBe: 3421)
-//        page2(input: smallInput, shouldBe: 36)
+        page2(input: smallInput, shouldBe: 36)
 //        page2(input: slightlyLargerInput, shouldBe: 103)
 //        page2(input: evenLargerExample, shouldBe: 3509)
-//        page2(input: input, shouldBe: 3421)
+        page2(input: input, shouldBe: 84870)
     }
     func page1(input: Input, shouldBe: Int) {
         let lines = input.map(Line12.init(line:))
@@ -45,9 +45,26 @@ final class Day12Tests: XCTestCase {
                 .filter{$0 != n}
             
         }
-        getPath(dic: nodeDic, canVisit: 2)
+        
+        func paths(_ n: String, visited: Set<String>, mult: Bool, cave: String) -> Int {
+            if n == END {return 1}
+            if n == START && !visited.isEmpty {return 0}
+            if n.isSmall && visited.contains(n) && !mult {
+                return 0
+            }
+            var cave = cave
+            if n.isSmall && visited.contains(n) && mult {
+                if cave == "" {cave = n}
+                else {return 0}
+            }
+            var poss = 0
+            for r in nodeDic[n]! {
+                poss += paths(r, visited: visited.union([n]), mult: mult, cave: cave)
+            }
+            return poss
+        }
           
-            .count.shouldBe(shouldBe)
+        paths(START, visited: Set(), mult: true, cave: "").shouldBe(shouldBe)
     }
     private func getPath(dic: [String: [String]], canVisit: Int) -> Set<[String]>{
         var paths: [[String]] = []
@@ -65,6 +82,7 @@ final class Day12Tests: XCTestCase {
         getSubRoute(START, visited: [START:canVisit], path: [])
         return paths.set()
     }
+
     func testFilter() throws {
         let sut = "start,A,b,A,b,A,c,A,c,A,end".components(separatedBy: ",")
         sut.filter(\.isSmall)
